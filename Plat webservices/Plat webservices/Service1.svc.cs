328c0e720f96de626a1db7c19cd6198a3226cdd9
Prakash Kandasamy <prakashyd@hotmail.com>
2022-04-06 22:27:56 +0530

    second change

--- Plat webservices/Plat webservices/Service1.svc.cs
+++ Plat webservices/Plat webservices/Service1.svc.cs
@@ -15,6 +15,7 @@ namespace Plat_webservices
         public string GetData(int value)
         {
             return string.Format("You entered: {0}", value);
+
         }
 
         public CompositeType GetDataUsingDataContract(CompositeType composite)
